dcf374007d454174b781224977f67fcbf7503950
marcospaulo <marcospaulomacedo@gmail.com>
2022-10-21 19:43:56 -0300

    exercícios da lista 10 de programação

---
+++ exercicios-10/exercicio1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//programa que imprima o seu nome;
+
+int main(){
+  char nome[15];
+  sprintf(nome,"Marcos Paulo");
+
+  printf("%s",nome);
+}
---
+++ exercicios-10/exercicio4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//programa que leia e imprima um número inteiro
+
+int main(){
+  int numero;
+
+  printf("Digite um numero inteiro: \n");
+  scanf("%i", &numero);
+
+  printf("%i",numero);
+}
